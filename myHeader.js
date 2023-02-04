9e2f177222ca29f775836f91994a03672f64f3d5
eusimar123 <122410803+eusimar123@users.noreply.github.com>
2023-02-03 21:35:59 -0300

    Aula do dia 01/02
    Atualizado 03/02/23

--- myHeader.js
+++ myHeader.js
@@ -1,38 +1,56 @@
+const myLinks = [
+    ["Inicio", "/"],
+    ["Teste", "/teste.html"],
+    ["Projeto 1", "/projects/projeto1.html"],
+    ["Currículo", "/curriculo.html"],
+    ["GitHub", "https://github.com/eusimar123"],
+    ["LinkedIn", "https://www.linkedin.com/in/eusimar-queiroz-702680253"]
+];
+
 class MyHeader extends HTMLElement{
     connectedCallback() {
         //createElement = cria uma elemento HTML baseado numa TAG especificada
         const header = document.createElement("header");
-        const nav = document.createElement("nav");
-        const ul = document.createElement("ul");
         const titleDiv = document.createElement("div");
         const pageTitle = document.createElement("h1");
 
-        const myLinks = [
-            ["Inicio", "/"],
-            ["Teste", "/teste.html"],
-            ["Projeto 1", "/projects/projeto1.html"]
-
-
-        ];
-
-        for(let i = 0; i < myLinks.length; i++){
-            let li = document.createElement("li");
-            let a = document.createElement("a");
-            a.setAttribute("href", myLinks[i][1]);
-            a.textContent = myLinks[i][0];
-            li.appendChild(a);
-            ul.appendChild(li);
-        }
-
-        nav.appendChild(ul);
         pageTitle.textContent = "Meu Site";
         titleDiv.appendChild(pageTitle);   
         header.appendChild(titleDiv);
-        header.appendChild(nav);
+        header.appendChild(navBar(myLinks));
         this.appendChild(header);
 
     }
 
 }
 
+function navBar(webpageList) {
+    //Essa função pega uma lista de sites, e gera uma barra de navegação
+    const nav = document.createElement("nav");
+    const ul = document.createElement("ul");
+
+    //Esse loop cria um item de lista com um link para cada um dos elementos do webpageList
+    for(let i = 0; i < webpageList.length; i++){
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        li.setAttribute("class", "mynavbar");
+        a.setAttribute("href", webpageList[i][1]);
+        a.textContent = webpageList[i][0];
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+
+    //adiciono minha lista não ordenada ao meu nav, e retorno meu nav
+    nav.appendChild(ul);  
+    return nav;    
+}
+
+function addCss(){
+    let myCss = document.createElement("link");
+    myCss.setAttribute("rel", "stylesheet");
+    myCss.setAttribute("href", "/estilo.css");
+    document.head.appendChild(myCss);
+}
+
+addCss();
 customElements.define("my-header", MyHeader);
